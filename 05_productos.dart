1bf676fc2d11ed1f68f05263e604c8f08935af70
Esteban4 <juanes.x777@gmail.com>
2021-07-29 19:04:28 -0500

    Ejercicio cinco terminado

--- 05_productos.dart
+++ 05_productos.dart
@@ -1,7 +1,8 @@
 //@dart=2.9
-/* 5. Se necesita crear un programa que permita ingresar los productos de 1 a N de un mercado 
-con su valor y cantidad dónde:
-• Se debe mostrar el total del mercado calculando cantidad y valor, teniendo en cuenta un IVA del 19%
+/* 5. Se necesita crear un programa que permita ingresar los productos 
+de 1 a N de un mercado con su valor y cantidad dónde:
+• Se debe mostrar el total del mercado calculando cantidad y valor, teniendo 
+en cuenta un IVA del 19%
 • Se debe mostrar el total bruto y neto, al igual que el valor del IVA
 • Se debe mostrar el producto más caro y el más barato.
 • Se debe crear una funcionalidad que permita listar todos los productos ordenados de mayor a menor 
@@ -11,10 +12,16 @@ import 'dart:io';
 
 main() {
   String producto;
-  double valor = 0;
-  double cantidad = 0;
+  double valorUnidad = 0.0;
+  double cantidad = 0.0;
   String continuar = 'y';
   List listado = [];
+  double valor = 0.0;
+  double iva = 0.0;
+  double valorBruto = 0.0;
+  double valorNeto = 0.0;
+  double total = 0.0;
+  List<double> listadoValor = [];
 
   while (continuar == 'y') {
     stdout.writeln('Por favor ingrese un nombre producto.');
@@ -24,12 +31,32 @@ main() {
     cantidad = double.parse(stdin.readLineSync());
 
     stdout.writeln('Por favor ingrese un valor.');
-    valor = double.parse(stdin.readLineSync());
+    valorUnidad = double.parse(stdin.readLineSync());
 
     print('Si desea continuar ingrese "y".');
     continuar = stdin.readLineSync();
 
-    listado = [producto, cantidad, valor];
+    valor = valorUnidad * cantidad;
+    listadoValor.add(valor);
+
+    listado.add(
+        'Producto: $producto, Cantidad: $cantidad,  Valor productos: $valor');
   }
-  print(listado);
+  for (var item in listadoValor) {
+    total = total + item;
+    iva = total * 0.19;
+    valorBruto = total - iva;
+    valorNeto = total;
+  }
+  listadoValor.sort((a, b) => b.compareTo(a));
+
+  for (var item in listado) {
+    print(item);
+  }
+  print('Valor bruto: $valorBruto');
+  print('Valor neto: $valorNeto');
+  print('Valor iva: $iva');
+  print('Lista de precios ordenada de mayor a menor: $listadoValor');
+  print('Producto de menor valor: ${listadoValor.last}');
+  print('Producto de mayor valor: ${listadoValor.first}');
 }
